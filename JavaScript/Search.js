9f58f7dc6586880a9b76a1e259ae8b4e325cacbf
Gustav Skogström <gustav.skogstrom@gmail.com>
2023-05-03 14:38:25 +0200

    1

---
+++ JavaScript/Search.js
@@ -0,0 +1,3 @@
+console.log(window.location.search);
+const id = window.location.search.split("=")[1]
+console.log(id);
--- JavaScript/workouts.js
+++ JavaScript/workouts.js
@@ -27,4 +27,40 @@ const listWorkouts = async () => {
 };
 listWorkouts();
 
+const one = document.querySelector('#Abdominals')
+
+const Abdominals = async () => {
+    
+    const url = 'http://localhost:3000/Workouts/1';
+
+
+    const response = await fetch(url);
+
+
+    if (response.status == 200) {
+
+        const data = await response.json();
+        console.log(data);
+        one.insertAdjacentHTML(
+            "beforeend",
+            `<div>
+            <h4>${data.name} Muscle: ${data.muscle}</h4>
+            <p>${data.reps}</p>
+            <p>${data.description}</p>
+            </div>`
+            );
+        // data.forEach((Workouts) => {
+        // one.insertAdjacentHTML(
+        // "beforeend",
+        // `<div>
+        // <h4>${Workouts.name} Muscle: ${Workouts.muscle}</h4>
+        // <p>${Workouts.reps}</p>
+        // <p>${Workouts.description}</p>
+        // </div>`
+        // );
+        // });
+    }
+};
+Abdominals();
+
 // json-server --watch Workouts.json
